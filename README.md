78c6bb2aff93bea59e63d961c5e6620f40eed207
Kanwarnoor Singh <famousfortech500@gmail.com>
2024-04-03 20:33:07 +0530

    Added Bubble sore in sorting algos

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Data Structures and Algorithms
 
 All the important code for different algorithms needed is available in this DSAP repository.
-testing 1
---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{   
+    int temp;
+
+    for(int i=0; i<=n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }  
+    }
+}
+
+void display(int arr[], int n){
+    for(int i=0; i<=n-1; i++){
+        cout<<arr[i]<<endl;
+    }
+}
+
+int main(void)
+{
+    int arr[10] = {10,9,8,7,6,5,4,3,2,1};
+    int n = sizeof(arr)/sizeof(int);
+
+    bubbleSort(arr, n);
+    display(arr, n);
+}
